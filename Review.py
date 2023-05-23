7abf758c4030643321e5c2b70f11b4f6fe5420b6
Xin Li <xli@crestron.com>
2023-05-23 15:37:33 -0400

    there we go

---
+++ Review.py
@@ -0,0 +1,27 @@
+import random
+
+class Node:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.visited = False
+
+def gen_grid(rows, cols)->list:
+    grid = [[] for i in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            cell = Node(j, i)
+            grid[i].append(cell)
+    return grid
+
+if __name__ == '__main__':
+    print(f'Hello and welcome!')
+    rows = input("How many rows do you want in your grid? ")
+    cols = input("How many columns do you want in your grid? ")
+    # print(f'{gen_grid(int(rows), int(cols))}')
+
+    grid = gen_grid(int(rows), int(cols))
+    for i in range(len(grid)):
+        for node in grid[i]:
+            print(f'{node.x, node.y}')
